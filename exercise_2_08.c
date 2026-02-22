ed019e9db00e1c49495e83fa731265e596281b61
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-02-22 08:54:16 +0100

    solved exercise 2.8

--- exercise_2_08.c
+++ exercise_2_08.c
@@ -1,1 +1,29 @@
-write a function rightrot(x,n) that returns the value of the integer c rotated to the right by n bit positions.
+#include <stdio.h>
+
+int rightrot(int x, int n);
+
+int main() {
+    int r = rightrot(0xA0000000, 2);
+    printf("%x\n", r);
+    return 0;
+}
+
+int rightrot(int x, int n) {
+    unsigned int mask = 1;
+    while (true) {
+        unsigned int new_mask = mask << 1;
+        if (new_mask != 0) {
+            mask = new_mask;
+        } else {
+            break;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        int bit = (x & mask) != 0;
+        x = (x << 1) | bit;
+    }
+
+    return x;
+}
+
