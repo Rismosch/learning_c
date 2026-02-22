2d79d05faaea8cfc034dbab27bd03b9444790bb0
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-02-22 08:44:48 +0100

    solved exercise 2.7

--- exercise_2_07.c
+++ exercise_2_07.c
@@ -1,1 +1,26 @@
-write a function invert(x,p,n) that returns x with the n bits that begin at position p inverters (ie, 1 changed into 0 and vice versa), leaving the others unchanged
+#include <stdio.h>
+
+// returns x with the n bits that begins at position p inverted
+// example:
+// x = 101101
+// p = 2
+// n = 3
+// 
+// result:
+//
+//       nnn
+// x    101101
+// x'   110001
+int invert(int x, int p, int n);
+
+int main() {
+    int r = invert(055, 2, 3);
+    printf("%o\n", r);
+    return 0;
+}
+
+int invert(int x, int p, int n) {
+    int mask = (1 << n) - 1;
+    return x ^ (mask << p);
+}
+
