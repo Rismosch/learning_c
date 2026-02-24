0a0b9715046114b8da51056c4acbc7507a81bcf0
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-02-24 15:24:00 +0100

    solved exercise 3.6

---
+++ exercise_3_06.c
@@ -0,0 +1,52 @@
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_LEN 1<<10
+
+/* itoa: convert n to characters in s using base b */
+void itoa(int n, char s[], int min_width);
+
+int main() {
+    char buf[BUF_LEN];
+    itoa(12345, buf, 2);
+    printf("%s\n", buf);
+    itoa(67890, buf, 10);
+    printf("%s\n", buf);
+}
+
+void reverse(char s[]) {
+    int c, i, j;
+
+    for (i = 0, j = strlen(s) - 1; i < j; ++i, --j) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itoa(int n, char s[], int min_width) {
+    int i;
+    long sign;
+
+    long l = n; 
+    if ((sign = l) < 0) {
+        l = -l;
+    }
+
+    i = 0;
+    do {
+        s[i++] = l % 10 + '0';
+    } while ((l /= 10) > 0);
+
+    if (sign < 0) {
+        s[i++] = '-';
+    }
+
+    while (i < min_width) {
+        s[i++] = ' ';
+    }
+
+    s[i] = '\0';
+    reverse(s);
+}
