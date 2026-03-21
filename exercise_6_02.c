d48ce43cac047f4fed069d32ed02ad1a1d74fe0c
Simons Thinkpad <simon.sutoris@googlemail.com>
2026-03-21 01:13:33 +0100

    stopped doing more exercises

---
+++ exercise_6_02.c
@@ -0,0 +1,5 @@
+// I have given up. string manipulation in C is hard, because
+// strings in C just suck. i decided that the exercises are not
+// worth my time anymore, since i want to go back to engine
+// development. i read through the rest of the book without
+// completing any more exercises.
